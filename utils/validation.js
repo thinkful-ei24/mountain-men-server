d54c65afce1a1c45dd04e98afa4a827d5a71aace
Sean Phelan <seanphelan97@gmail.com>
2018-12-07 11:34:40 -0700

    fix middleware issue

--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,6 @@
 const middlewareValidators = {
   requireFields: (reqFields) => (req, res, next) => {
+    console.log('require fields');
     const missingField = reqFields.find(field => !(field in req.body));
     if (missingField) {
       return res.status(422).json({
@@ -9,6 +10,24 @@ const middlewareValidators = {
         location: missingField
       });
     }
+    return next();
+  },
+
+  trimmedFields: (trimmedFields) => (req, res, next) => {
+    console.log('require trimmed fields');
+    const nonTrimmedField = trimmedFields.find(
+      field => req.body[field].trim() !== req.body[field]
+    );
+  
+    if (nonTrimmedField) {
+      return res.status(422).json({
+        code: 422,
+        reason: "ValidationError",
+        message: "Cannot start or end with whitespace",
+        location: nonTrimmedField
+      });
+    }
+    return next();
   }
 };
 
