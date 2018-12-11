f7e01d8d757989aeb2bcbdfec80169bd826beb9f
Sean Phelan <seanphelan97@gmail.com>
2018-12-11 12:50:07 -0700

    add PUT endpoint (does not check for null/undefined fields)

--- routes/profile.js
+++ routes/profile.js
@@ -14,7 +14,6 @@ router.get('/:id', (req, res, next) => {
       delete profileInfo.address;
       delete profileInfo.phoneNumber;
       delete profileInfo.email;
-      delete profileInfo.id; // might as well
       res.json(profileInfo);
     })
     .catch(err => {
@@ -27,40 +26,27 @@ router.use(
   passport.authenticate("jwt", { session: false, failWithError: true })
 );
 
-// email: {type: String, required: true, unique: true},
-// password: {type: String, required: true},
-// firstName: {type: String, required: true},
-// lastName: {type: String, required: true},
-// phoneNumber: {type: String, required: true},
-// address: {type: String, required: true},
-// type: {type: String, enum: ['DRIVER', 'USER'], default: 'USER', required: true}
 router.put('/:id', (req, res, next) => {
-  const {id: userId} = req.user.id;
-  Object.keys(req.body).map(key => {
-    console.log('key: ', key);
-    console.log(typeof key);
-  });
-  const changedFields = {
-    // email: req.body.email,
-    // firstName: req.body.firstName,
-    // lastName: 
+  const {id: userId} = req.user;
+  const {email, firstName, lastName, phoneNumber, address, type} = req.body;
+
+  const updatedObject = {
+    email, firstName, lastName, phoneNumber, address, type
   };
-  // const {}
-  // FIXME: check if id matches auth id
+
+  // FIXME: update id check
   if(userId !== req.params.id) {
     const err = new Error('User ids do not match');
     err.status = 403;
     return next(err);
   }
 
-  User.findOneAndUpdate({_id: userId}, changedFields, {new: true})
+  User.findOneAndUpdate({_id: userId}, updatedObject, {new: true})
     .then(dbRes => {
-      console.log(dbRes);
       return res.json(dbRes);
     })
     .catch(err => {
       err.status = 400;
-      console.log(err);
       return next(err);
     });
 });
