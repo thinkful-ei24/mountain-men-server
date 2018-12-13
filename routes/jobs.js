cc1ff5b5d42f98f70879596489c8fd9c0770fe29
Aaron Whitehead <whitehead.aaron@live.com>
2018-12-13 15:48:40 -0700

    fixed post end point

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const passport = require("passport");
 const mongoose = require("mongoose");
-const axios = require('axios');
+const axios = require("axios");
 const Joi = require("joi");
 
-const {GEOCODE_URL} = require('../config');
-const {requireFields} = require('../utils/validation');
-const {formatValidateError} = require('../utils/validate-normalize');
-const Post = require('../models/post');
-const User = require('../models/user');
+const { GEOCODE_URL } = require("../config");
+const { requireFields } = require("../utils/validation");
+const { formatValidateError } = require("../utils/validate-normalize");
+const Post = require("../models/post");
+const User = require("../models/user");
 
 const app = express();
 
 // description ok as alphanumeric?
 const postSchema = Joi.object().keys({
-  title: Joi.string().min(3).max(40).required(),
+  title: Joi.string()
+    .min(3)
+    .max(40)
+    .required(),
   description: Joi.string().max(400),
-  date: Joi.string()
+  date: Joi.string(),
+  userId: Joi.string(),
+  accepted: Joi.boolean(),
+  acceptedUserId: Joi.string(),
+  coords: Joi.object()
 });
 
 // unprotected endpoints
@@ -29,16 +36,17 @@ app.get("/", (req, res, next) => {
 });
 
 // protected
-app.get('/:id', (req, res, next) => {
+app.get("/:id", (req, res, next) => {
   const userId = req.params.id;
-  if(!mongoose.Types.ObjectId.isValid(userId)) {
-    const err = new Error('Path is not a valid user id');
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error("Path is not a valid user id");
     return next(err);
   }
-  return Post.find({userId})
+  return Post.find({ userId })
     .then(dbRes => {
       return res.json(dbRes).status(200);
-    }).catch(err => {
+    })
+    .catch(err => {
       return next(err);
     });
 });
@@ -48,59 +56,65 @@ app.use(
   passport.authenticate("jwt", { session: false, failWithError: true })
 );
 
-const jobPostFields = ["title", "description", "date", "city", "state", "zip", "address"];
-app.post('/:id', requireFields(jobPostFields), (req, res, next) => {
-
-  if(req.user.id !== req.params.id) {
-    const err = new Error('Unauthorized to post a job for this user');
+const jobPostFields = ["title", "description", "date"];
+app.post("/:id", requireFields(jobPostFields), (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    const err = new Error("Unauthorized to post a job for this user");
     err.status = 401;
     return next(err);
   }
 
-  let postData;
-  // shouldn't have to look up the user id in the db because it's matched against auth
-  return Joi.validate(req.body, postSchema)
-    .then(obj => {
-      postData = {
-        title: obj.title,
-        description: obj.description,
-        date: obj.date,
-        userId: req.user.id,
-        accepted: false,
-        acceptedUserId: null
-      };
+  let postData = {
+    title: req.body.title,
+    description: req.body.description,
+    date: req.body.date,
+    userId: req.user.id,
+    accepted: false,
+    acceptedUserId: 'string'
+  };
+
+  const { city, state, zipCode, street } = req.body;
+  const geocodeStr = street + " " + city + " " + state + " " + zipCode;
+  console.log(geocodeStr, postData);
+  axios
+    .get(GEOCODE_URL, {
+      params: {
+        address: geocodeStr,
+        key: process.env.MAPS_API_KEY
+      }
     })
-    .catch(joiError => next(formatValidateError(joiError)))
-    // get latitude and longitude from maps api
-    .then(() => {
-      const {city, state, zip, address} = req.body;
-      const geocodeStr = encodeURI(address + ' ' + city + ' ' + state + ' ' + zip);
-      return axios.get(GEOCODE_URL, {
-        params: {
-          address: geocodeStr,
-          key: process.env.MAPS_API_KEY
-        }
-      });
+    .then(apiRes => {
+      console.log('here')
+      const { lat, lng } = apiRes.data.results[0].geometry.location;
+      postData.coords = { lat, lng };
+
+      // shouldn't have to look up the user id in the db because it's matched against auth
+      Joi.validate(postData, postSchema)
+        .then(() => {
+          console.log(postData);
+          return Post.create(postData);
+        })
+        .then(dbRes => {
+          return res
+            .location(`${req.originalUrl}/${dbRes.id}`)
+            .status(201)
+            .json(dbRes);
+        })
+        .catch(joiError => {
+          console.log("joi", joiError);
+          next(formatValidateError(joiError));
+        });
+      // get latitude and longitude from maps api
     })
-    // create post
-    .then(res => {
-      const {lat, lng} = res.data.results[0].geometry.location;
-      postData.coords = {lat, lng};
-      return Post.create(postData);
-    })
-    // send post
-    .then(dbRes => {
-      return res
-        .location(`${req.originalUrl}/${dbRes.id}`)
-        .status(201)
-        .json(dbRes);
-    })
-    .catch(err => next(err));
+
+    .catch(err => {
+      console.log("error", err);
+      next(err);
+    });
 });
 
 app.put("/:userId/:jobId", (req, res, next) => {
-  
-  const {userId, jobId} = req.params;
+  const { userId, jobId } = req.params;
 
   const newObj = {};
 
@@ -114,7 +128,6 @@ app.put("/:userId/:jobId", (req, res, next) => {
   if (req.body.acceptedUserId) {
     newObj.acceptedUserId = req.body.acceptedUserId;
   }
-
 
   return Post.findOneAndUpdate({ _id: jobId, userId }, newObj, { new: true })
     .then(result => {
