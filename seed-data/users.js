576ef20e9759169c9dc6112fc1c6c8679c43f9c4
Sean Phelan <seanphelan97@gmail.com>
2018-12-14 14:48:01 -0700

    add basic password management

--- seed-data/users.js
+++ seed-data/users.js
@@ -6,7 +6,8 @@ const users = [
     phoneNumber: '2025550180',
     address: '600 South Fairground Ave',
     _id: '000000000000000000000001',
-    type: 'USER'
+    type: 'USER',
+    password: '$2a$10$Qrr9jP8/.Ld0mOkxcHtcR./OwKx2AFzau59uksXpdp0Tw1F6i6wgu'
   },
   {
     email: 'werodeintrucks@gmail.com',
@@ -15,7 +16,8 @@ const users = [
     phoneNumber: '2025550168',
     address: '637 Richardson Drive',
     _id: '000000000000000000000002',
-    type: 'USER'
+    type: 'USER',
+    password: '$2a$10$BRu9S07Y1ZArQ9G/Vpr.w.eykrlm6McZEzXNN99C5M/0ynCyoFbAS'
   },
   {
     email: 'idriveyourtruck@gmail.com',
@@ -24,7 +26,8 @@ const users = [
     phoneNumber: '2025550148',
     address: '361 Ann Ave',
     _id: '000000000000000000000003',
-    type: 'DRIVER'
+    type: 'DRIVER',
+    password: '$2a$10$GvqiYAYuyu0QKf8SlIynMu1oXi8LNpHggMJ5a10vhXtSTFs0bKd4a'
   },
   {
     email: 'pickupman@gmail.com',
@@ -33,7 +36,8 @@ const users = [
     phoneNumber: '2025550168',
     address: '8335 North Bow Ridge Drive',
     _id: '000000000000000000000004',
-    type: 'DRIVER'
+    type: 'DRIVER',
+    password: '$2a$10$2VdT.O19Qfktj5I5/dw.iOhJBvSNpvcpumXN.XiFBZbrNoBuryrGq'
   }
 ];
 
---
+++ utils/hash-password.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcryptjs');
+const password = 'password';
+
+bcrypt.hash(password, 10)
+  .then(digest => {
+    console.log('digest = ', digest);
+    return digest;
+  })
+  .catch(err => {
+    console.error('error', err);
+  });
