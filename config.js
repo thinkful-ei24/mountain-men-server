0fb043e37c34684e1be0efef742a489c14ddf8e4
Sean Phelan <seanphelan97@gmail.com>
2018-12-17 16:41:57 -0700

    jwt secret fix

--- config.js
+++ config.js
@@ -2,6 +2,10 @@
 
 require('dotenv').config();
 
+if(!process.env.JWT_SECRET) {
+  throw Error('JWT_SECRET environment variable is not set.');
+}
+
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
@@ -9,6 +13,6 @@ module.exports = {
     process.env.DATABASE_URL || "mongodb://localhost",
   DATABASE_NAME: process.env.DATABASE_NAME || "truckd-db",
   TEST_DATABASE_NAME: process.env.TEST_DATABASE_NAME || "truckd-test-db",
-  JWT_SECRET: process.env.JWT_SECRET || ".env.JWT_SECRET",
+  JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || "7d"
 };
